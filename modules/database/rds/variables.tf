d9b78bb537d92467cce9fbfb2f1f17264c71c063
yun1270 <modo000127@gmail.com>
2025-11-05 17:12:38 +0900

    feat(db): modules/database/rds - main.tf, output.tf, variables.tf 추가

---
+++ modules/database/rds/variables.tf
@@ -0,0 +1,39 @@
+variable "name" { type = string }
+variable "common_tags" {
+  type = map(string)
+  default = {}
+}
+
+# rds subnet
+variable "private_subnet_ids" {type = list(string)}
+
+# rds instance
+variable "db_username" { type = string }
+variable "db_password" { type = string }
+variable "db_name" {
+  type    = string
+  default = ""
+}
+variable "engine_version" {
+  type    = string
+  default = "8.0"
+}
+variable "instance_class" {
+  type    = string
+  default = "db.m5.large"
+}
+variable "rds_backup_day" { type = number }
+variable "sg_rds_id" {type = string}
+
+# rds proxy
+variable "proxy_idle_client_timeout" { 
+  type = number 
+  default = 1800
+}
+variable "proxy_role_arn" { type = string }
+variable "rds_proxy_secret_arn" { type = string }
+variable "proxy_borrow_timeout" { 
+  type = number 
+  default = 120
+}
+variable "sg_rds_proxy_id" {type = string}
