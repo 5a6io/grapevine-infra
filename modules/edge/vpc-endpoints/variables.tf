67497bcff3ef66cdb8ac080c40a84e4d5f9b28de
yun1270 <modo000127@gmail.com>
2025-11-05 18:28:10 +0900

    feat(vpce): modules/edge/vpc-endpoint - main.tf, variables.tf

---
+++ modules/edge/vpc-endpoints/variables.tf
@@ -0,0 +1,10 @@
+variable "name" { type = string }
+variable "common_tags" {
+  type = map(string)
+  default = {}
+}
+
+variable "region"  { type = string }
+variable "vpc_id"     { type = string }
+variable "private_subnet_ids" { type = list(string) }
+variable "private_route_table_ids" { type = list(string) }
