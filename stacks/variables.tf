084035840e3f63bc5226016b6ad795ec0ed80004
yun1270 <modo000127@gmail.com>
2025-11-05 17:20:05 +0900

    feat(infra): stacks/.. - 파일 수정

--- stacks/variables.tf
+++ stacks/variables.tf
@@ -27,3 +27,18 @@ variable "multi_nat" {
   type        = bool
   default     = false
 }
+
+# Database
+# rds instance
+variable "db_username" { type = string }
+variable "db_password" { type = string }
+variable "db_name" { type = string }
+variable "engine_version" { type = string }
+variable "instance_class" { type = string }
+variable "rds_backup_day" { type = number }
+
+# rds proxy
+variable "proxy_idle_client_timeout" { type = number }
+variable "proxy_role_arn" { type = string }
+variable "rds_proxy_secret_arn" { type = string }
+variable "proxy_borrow_timeout" { type = number }
