6dffae18511acaf672f4aa7cf7e90ca81420f637
yun1270 <modo000127@gmail.com>
2025-11-06 18:55:27 +0900

    chore(vpce): modules/security/security-group, modules/edge/vpc-endpoint - vpc endpoint security-group 분리

--- modules/edge/vpc-endpoints/main.tf
+++ modules/edge/vpc-endpoints/main.tf
@@ -1,29 +1,3 @@
-# VPC Endpoint
-resource "aws_security_group" "vpc_endpoint_sg" {
-  name   = "${var.name}-vpce-sg"
-  vpc_id = var.vpc_id
-
-  ingress {
-    description = "HTTPS from VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(var.common_tags, {
-    Name = "${var.name}-vpce-sg"
-  })
-}
-
-
 # S3 (Gateway) - 라우트 테이블 연결
 resource "aws_vpc_endpoint" "s3_gw" {
   vpc_id            = var.vpc_id
@@ -40,7 +14,6 @@ resource "aws_vpc_endpoint_route_table_association" "s3_gw_assoc" {
   vpc_endpoint_id = aws_vpc_endpoint.s3_gw.id
 }
 
-
 # 1) ECR API
 resource "aws_vpc_endpoint" "ecr_api" {
   vpc_id              = var.vpc_id
@@ -48,7 +21,7 @@ resource "aws_vpc_endpoint" "ecr_api" {
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
   subnet_ids          = var.private_subnet_ids
-  security_group_ids  = [aws_security_group.vpc_endpoint_sg.id]
+  security_group_ids  = [var.sg_vpce_id]
   tags = merge(var.common_tags, {
     Name = "${var.name}-vpce-ecr-api"
   })
@@ -61,7 +34,7 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
   subnet_ids          = var.private_subnet_ids
-  security_group_ids  = [aws_security_group.vpc_endpoint_sg.id]
+  security_group_ids  = [var.sg_vpce_id]
   
   tags = merge(var.common_tags, {
     Name = "${var.name}-vpce-ecr"
@@ -75,7 +48,7 @@ resource "aws_vpc_endpoint" "secretsmanager" {
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
   subnet_ids          = var.private_subnet_ids
-  security_group_ids  = [aws_security_group.vpc_endpoint_sg.id]
+  security_group_ids  = [var.sg_vpce_id]
   tags = merge(var.common_tags, {
     Name = "${var.name}-vpce-secretsmanager"
   })
@@ -88,7 +61,7 @@ resource "aws_vpc_endpoint" "kms" {
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
   subnet_ids          = var.private_subnet_ids
-  security_group_ids  = [aws_security_group.vpc_endpoint_sg.id]
+  security_group_ids  = [var.sg_vpce_id]
   tags = merge(var.common_tags, {
     Name = "${var.name}-vpce-kms"
   })
@@ -101,7 +74,7 @@ resource "aws_vpc_endpoint" "logs" {
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
   subnet_ids          = var.private_subnet_ids
-  security_group_ids  = [aws_security_group.vpc_endpoint_sg.id]
+  security_group_ids  = [var.sg_vpce_id]
   tags = merge(var.common_tags, {
     Name = "${var.name}-vpce-logs"
   })
