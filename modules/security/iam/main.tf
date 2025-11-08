3f32c8350dd2b572abebbb94e87589d553e9185b
yun1270 <modo000127@gmail.com>
2025-11-09 03:46:51 +0900

    fix(iam): modules/security/iam - ecs_task_execution_role.assume_role_policy 수정   - Action =. Action = "sts.AssumeRole" -> "sts:AssumeRole"

--- modules/security/iam/main.tf
+++ modules/security/iam/main.tf
@@ -4,15 +4,13 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-        {
-            Action = "sts.AssumeRole"
-            Effect = "Allow"
-            Principal = {
-                Service = "ecs-tasks.amazonaws.com"
-            }
-        }
-    ]
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
   })
 
   tags = merge(var.common_tags, {
