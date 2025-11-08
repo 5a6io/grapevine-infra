7ef6dc6bf48bcc543fb1a09f17319d9af739833d
yun1270 <modo000127@gmail.com>
2025-11-09 02:12:35 +0900

    chore(cicd): stacks/11-codedeploy.tf - services = module.ecs_service.service_names -> services = var.services

---
+++ stacks/11-codedeploy.tf
@@ -0,0 +1,14 @@
+module "codedeploy" {
+  source = "../modules/cicd/codedeploy"
+  name = var.name
+  common_tags = var.common_tags
+
+  target_group_blue = module.alb.target_groups_blue_arns
+  target_group_green = module.alb.target_group_green_arns
+  lb_listener_arns = module.alb.alb_listener_https_arns
+  
+  ecs_cluster_name = module.ecs_cluster.ecs_cluster_name
+  # services = module.ecs_service.service_names
+  services = var.services
+  codedeploy_role_arn = module.iam.codedeploy_role
+}
