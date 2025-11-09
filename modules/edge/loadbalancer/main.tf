424a20e0aa9a899992c49528468b255becb16d80
yun1270 <modo000127@gmail.com>
2025-11-09 11:46:04 +0900

    chore(lb): modules/edge/loadbalancer - https 다시 추가

--- modules/edge/loadbalancer/main.tf
+++ modules/edge/loadbalancer/main.tf
@@ -21,15 +21,10 @@ resource "aws_lb_listener" "http_listener" {
     type = "redirect"
 
     redirect {
-      port = 80
-      protocol = "HTTP"
+      port = 443
+      protocol = "HTTPS"
       status_code = "HTTP_301"
     }
-    # redirect {
-    #   port = 443
-    #   protocol = "HTTPS"
-    #   status_code = "HTTP_301"
-    # }
   }
 
   tags = merge(var.common_tags, {
@@ -37,23 +32,23 @@ resource "aws_lb_listener" "http_listener" {
   })
 }
 
-# resource "aws_lb_listener" "https_listener" {
-#   for_each = var.services
-#   load_balancer_arn = aws_lb.this.arn
-#   port  = 443
-#   protocol = "HTTPS"
-#   ssl_policy = "ELBsecurity-2016-08"
-#   certificate_arn = var.alb_certificate_arn
+resource "aws_lb_listener" "https_listener" {
+  for_each = var.services
+  load_balancer_arn = aws_lb.this.arn
+  port  = 443
+  protocol = "HTTPS"
+  ssl_policy = "ELBsecurity-2016-08"
+  certificate_arn = var.alb_certificate_arn
   
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.blue[each.key].arn
-#   }
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.blue[each.key].arn
+  }
 
-#   tags = merge(var.common_tags, {
-#     Name = "${var.name}-lb-https-listener"
-#   })
-# }
+  tags = merge(var.common_tags, {
+    Name = "${var.name}-lb-https-listener"
+  })
+}
 
 resource "aws_lb_listener_rule" "this" {
     for_each = var.alb_certificate_arn != null ? var.services : {}
