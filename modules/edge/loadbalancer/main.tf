2c648300ebf1680c56f103e208cbf7ab61492ace
5a6io <yhm54321@naver.com>
2025-11-09 12:26:09 +0900

    chore/all: error

--- modules/edge/loadbalancer/main.tf
+++ modules/edge/loadbalancer/main.tf
@@ -59,7 +59,7 @@ resource "aws_lb_listener" "http_listener" {
 resource "aws_lb_listener_rule" "this" {
     # for_each = var.alb_certificate_arn != null ? var.services : {}
     # listener_arn = aws_lb_listener.https_listener[each.key].arn
-    listener_arn = aws_lb_listener.http_listener.arn
+    listener_arn = aws_lb_listener.http_listener[each.key].arn
     priority = index(local.service_keys, each.key) + 1
   
     condition {
