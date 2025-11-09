1b09757470aa9e693dd63375219acd66b4449f67
yun1270 <modo000127@gmail.com>
2025-11-09 16:16:15 +0900

    chore(monitortring): module "cloudwatch" 주석 처리

--- stacks/07-monitoring.tf
+++ stacks/07-monitoring.tf
@@ -1,10 +1,10 @@
-module "cloudwatch" {
-  source = "../modules/monitoring"
-  name = var.name
-  common_tags = var.common_tags
+# module "cloudwatch" {
+#   source = "../modules/monitoring"
+#   name = var.name
+#   common_tags = var.common_tags
 
-  region = var.region
-  ecs_cluster_name = module.ecs_cluster.ecs_cluster_name
-  log_groups = var.log_groups
-  cloudwatch_ecs_log_group = module.ecs_task_definition.cloudwatch_ecs_log_group_arns
-}
+#   region = var.region
+#   ecs_cluster_name = module.ecs_cluster.ecs_cluster_name
+#   log_groups = var.log_groups
+#   cloudwatch_ecs_log_group = module.ecs_task_definition.cloudwatch_ecs_log_group_arns
+# }
