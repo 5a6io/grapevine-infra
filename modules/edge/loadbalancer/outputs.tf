2ffcf628c8e8ea202925e424537098e689cd1cf5
5a6io <yhm54321@naver.com>
2025-11-09 12:31:51 +0900

    chore/all: error

--- modules/edge/loadbalancer/outputs.tf
+++ modules/edge/loadbalancer/outputs.tf
@@ -6,6 +6,10 @@ output "alb_dns_name" {
   value = aws_lb.this.dns_name
 }
 
+output "alb_listener_http_arns" {
+   value = { for k, v in aws_lb_listener.http_listener : k => v.arn}
+}
+
 # output "alb_listener_https_arns" {
 #   value = { for k, v in aws_lb_listener.https_listener : k => v.arn }
 # }
